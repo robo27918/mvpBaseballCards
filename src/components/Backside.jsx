62450cb0b4772c00bc1b51cc4d9ba47041adba8a
Roberto Toribio <roberttoribio04@gmail.com>
2023-09-13 12:04:41 -0700

    Backside Component added and Flipcard modified

---
+++ src/components/Backside.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+/**
+ * the backside of the players card which will display
+ * the players batting avg, hrs, rbis and OPS
+ */
+
+const Backside = ()=> {
+
+    return (
+        <div className="Back">
+            <h4>This is the backside component</h4>
+            <h4>Batting Average: .XXX</h4>
+            <h4>Homeruns: X</h4>
+            <h4> RBI: X</h4>
+            <h4>OPS: .XXX</h4>
+           
+            
+        </div>
+    );
+}
+export default Backside;
--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,10 +1,42 @@
+import React,{useState} from "react";
+import ReactCardFlip from "react-card-flip";
+/***
+ *  This was originally returned by the FlipCard component. Here for
+ *  reference only.....
+ *   <div>
+            Front of card
+            <button onClick={handleClick}>Click to flip</button>
+          </div>
 
-const FlipCard = ({onClick}) => {
+          <div>
+              This is the back of the Card
+              <button onClick={handleClick}>Click to flip</button>
+         </div>
+ */
+const FlipCard = ({front, back})=>{
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () =>{
+        setIsFlipped((prevIsFlipped) => !prevIsFlipped);
+    };
+    console.log("From the FlipCard component")
     return (
-        <div className="flipCard" onClick={onClick}>
-            <div className="card-front"> Front of card</div>
-            <div className="card-back">Back of card</div>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection={"horizontal"}>
+        <div>
+          {front}
+          <button onClick={handleClick}>Click to flip</button>
         </div>
+        
+
+        <div>
+          {back}
+          <button onClick={handleClick}>Click to flip</button>
+        </div>
+        
+        
+        
+
+        </ReactCardFlip>
     );
 }
 export default FlipCard;
